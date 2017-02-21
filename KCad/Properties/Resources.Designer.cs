3a3c97f387c84ea9242807b057a7f499352e8961
Tsuyoshi <tsu@jc4.so-net.ne.jp>
2017-02-21 23:17:58 +0900

    Add custom toggle button

--- KCad/Properties/Resources.Designer.cs
+++ KCad/Properties/Resources.Designer.cs
@@ -59,5 +59,15 @@ namespace KCad.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   型 System.Drawing.Bitmap のローカライズされたリソースを検索します。
+        /// </summary>
+        internal static System.Drawing.Bitmap xy {
+            get {
+                object obj = ResourceManager.GetObject("xy", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
