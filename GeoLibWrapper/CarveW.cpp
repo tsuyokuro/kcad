53599bb6d51ab0f6b1f1aafe894bd445dd88c9a6
Tsuyoshi <tsu@jc4.so-net.ne.jp>
2018-05-02 17:30:47 +0900

    Refactor
    CR/LF etc.

--- GeoLibWrapper/CarveW.cpp
+++ GeoLibWrapper/CarveW.cpp
@@ -1,11 +1,22 @@
 #include "CarveW.h"
+#include "geometry.hpp"
 
-
+using namespace CadDataTypes;
 
 namespace CarveW
 {
 	CarveW::CarveW()
 	{
+	}
+
+	CadMesh^ CarveW::CrateCylinder(
+		int slices,
+		double rad,
+		double height)
+	{
+		CadMesh^ ret = gcnew CadMesh();
+	
+		return ret;
 	}
 }
 
--- GeoLibWrapper/CarveW.h
+++ GeoLibWrapper/CarveW.h
@@ -1,11 +1,14 @@
 #pragma once
 
+using namespace CadDataTypes;
+
 namespace CarveW
 {
 	ref class CarveW
 	{
 	public:
 		CarveW();
+		CadMesh ^ CrateCylinder(int slices, double rad, double height);
 	};
 }
 
