e3eb6eb600e081d1cf99a6eeec687615729a12cb
Tsuyoshi <tsu@jc4.so-net.ne.jp>
2019-03-20 20:32:27 +0900

    Divided classes definition files

---
+++ CadDataTypes/CadDataTypes/CadFace.cs
@@ -0,0 +1,28 @@
+﻿using MyCollections;
+
+namespace CadDataTypes
+{
+    public class CadFace
+    {
+        public FlexArray<int> VList;
+
+        public CadFace()
+        {
+            VList = new FlexArray<int>(3);
+        }
+
+        public CadFace(params int[] args)
+        {
+            VList = new FlexArray<int>(args.Length);
+            for (int i=0; i< args.Length; i++)
+            {
+                VList.Add(args[i]);
+            }
+        }
+
+        public CadFace(CadFace src)
+        {
+            VList = new FlexArray<int>(src.VList);
+        }
+    }
+}
