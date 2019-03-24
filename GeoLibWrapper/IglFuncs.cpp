daeeb5153f6cbb3729653dfaa11cc294bac58b3c
Tsuyoshi <tsu@jc4.so-net.ne.jp>
2019-03-24 19:06:49 +0900

    Remove unnecessary code from native code

--- GeoLibWrapper/IglFuncs.cpp
+++ GeoLibWrapper/IglFuncs.cpp
@@ -1,6 +1,7 @@
 #pragma unmanaged
 #define _CRT_SECURE_NO_WARNINGS
 
+/*
 #include <igl/readOFF.h>
 #include <igl/cotmatrix.h>
 #include <igl/triangle/triangulate.h>
@@ -10,50 +11,12 @@
 #include <Eigen/Geometry>
 
 #include <iostream>
+*/
 
 #include "IglFuncs.h"
 
-
+// CLRが有効だとBuild errorになるようなコードは、こちらに書くようにする
+// 例えば、 #include <thread> を使うようなコード
 IglFuncs::IglFuncs()
 {
-}
-
-Eigen::MatrixXd IglFuncs::ArrayToMatrixXd(double * data, int rows, int cols)
-{
-	Eigen::MatrixXd m(rows, cols);
-
-	int p = 0;
-
-	for (int r = 0; r < rows; r++)
-	{
-		for (int c = 0; c < cols; c++)
-		{
-			m(r, c) = data[p++];
-		}
-	}
-
-	return m;
-}
-
-void IglFuncs::ReadOFF(
-	const char* fname,
-	Eigen::MatrixXd& V,
-	Eigen::MatrixXi& F)
-{
-	igl::readOFF(fname, V, F);
-}
-
-void IglFuncs::Test()
-{
-}
-
-void IglFuncs::triangulate(
-	const Eigen::MatrixXd & V,
-	const Eigen::MatrixXi & E,
-	const Eigen::MatrixXd & H,
-	const std::string flags,
-	Eigen::MatrixXd & V2,
-	Eigen::MatrixXi & F2)
-{
-	igl::triangle::triangulate(V, E, H, flags, V2, F2);
 }
--- GeoLibWrapper/IglFuncs.h
+++ GeoLibWrapper/IglFuncs.h
@@ -1,26 +1,9 @@
 #pragma once
 #pragma unmanaged
 
-#include <Eigen/Core>
-
 class IglFuncs
 {
 public:
 	IglFuncs();
-
-	static Eigen::MatrixXd ArrayToMatrixXd(double* data, int rows, int cols);
-	static void ReadOFF(const char* fname, Eigen::MatrixXd & V, Eigen::MatrixXi & F);
-
-
-	static void Test();
-
-	static void triangulate(
-		const Eigen::MatrixXd & V,
-		const Eigen::MatrixXi & E,
-		const Eigen::MatrixXd & H,
-		const std::string flags,
-		Eigen::MatrixXd & V2,
-		Eigen::MatrixXi & F2);
-	
 };
 
