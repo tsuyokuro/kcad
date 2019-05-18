9f4d5e06080c787e5c449502fb64fc74943e6a4a
Tsuyoshi <tsu@jc4.so-net.ne.jp>
2019-05-18 19:23:03 +0900

    Add test app

---
+++ TestApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using OpenTK;
+using Plotter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector3d v = default;
+
+            VectorUtil.Set(out v, 1, 2, 3);
+
+            Console.WriteLine("end");
+        }
+    }
+}
